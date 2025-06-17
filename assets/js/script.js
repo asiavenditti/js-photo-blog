646ee8fca07e35116834d0d5c71675c812c715dd
Your Name <you@example.com>
2025-06-17 16:18:18 +0200

    Refactor CSS styles and add JavaScript for dynamic photo loading

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+/* Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto! */
+
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+
+// effettuo una chiamata AJAX
+
+fetch(endpoint)
+
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+
+        data.forEach((card) => {
+
+            const { date, title, url } = card
+
+            const rowEl = document.querySelector('.row')
+            const cardMarkup = `<div class="col-4 col-md-6 col-sm-12">
+                    <div class="card">
+                        <img src="${url}" alt="yoga" class="card-img">
+                        <div class="card-body">
+                        <p> ${title}</p>
+                        <p> ${date}</p>
+                        </div>
+                        <div class="pin"><img src="./assets/img/pin.svg" alt="pin"></div>
+                    </div>
+                </div>`
+
+            rowEl.insertAdjacentHTML('afterbegin', cardMarkup)
+        })
+    })
